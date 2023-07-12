aff1f742eda54fc4f65687381425645f033b4d54
Tasneem Patrawala <tasneemp_sa@yahoo.com>
2023-07-12 08:24:06 -0400

    Add components Forward counter, Backward counter and card

---
+++ src/components/BackwardCounter.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from "react";
+import Card from "./Card";
+
+const BackwardCounter = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount(prevCount => prevCount - 1);
+        }, 1000)
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+    return <Card>{count}</Card>
+}
+
+export default BackwardCounter;
